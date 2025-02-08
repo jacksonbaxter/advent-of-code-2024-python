4967c090febd6d8f6f6adc7f715ca48a07a3d8f6
jacksonbaxter <jackson@jacksonbaxter.xyz>
2025-02-08 12:23:12 -0700

    day2p1

---
+++ day2/day2.py
@@ -0,0 +1,44 @@
+def read_input(filename):
+    with open(filename, 'r') as file:
+        reports = file.read().strip().split('\n')
+
+    reports = [list(map(int, line.split())) for line in reports]
+    
+    return reports
+
+def is_safe_report(levels):
+    if len(levels) <= 1:
+        return False
+
+    increasing = levels[1] > levels[0]
+
+    for i in range(1, len(levels)):
+        diff = levels[i] - levels[i - 1]
+
+        if abs(diff) < 1 or abs(diff) > 3:
+            return False
+    
+        if increasing and diff <= 0:
+            return False
+        if not increasing and diff >= 0:
+            return False
+
+    return True
+
+def calculate_safe(reports):
+    safe_count = 0
+    for report in reports:
+        if is_safe_report(report):
+            safe_count += 1
+
+    return safe_count
+
+def main():
+    reports = read_input('input.txt')
+
+    safe_count = calculate_safe(reports)
+
+    print(f"The total number of safe reports is: {safe_count}")
+
+if __name__ == "__main__":
+    main()
