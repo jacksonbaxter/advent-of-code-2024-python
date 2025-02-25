a8dd2b97846c15cf0e2dcaaf1ca56367a61a266a
jacksonbaxter <jackson@jacksonbaxter.xyz>
2025-02-25 11:36:12 -0700

    day5p2

--- day5/day5.py
+++ day5/day5.py
@@ -23,6 +23,15 @@ def is_valid(rules, update):
                 return False
     return True
 
+def fix_update(rules, update):
+    filtered_rules = defaultdict(set)
+    for i in update:
+        filtered_rules[i] = rules[i] & set(update)
+
+    ordered_keys = sorted(filtered_rules, key = lambda k: len(filtered_rules[k]), reverse = True)
+
+    return ordered_keys
+
 def part1(input):
     rules, updates = extract_data(input)
     count = 0
@@ -33,12 +42,25 @@ def part1(input):
 
     return count
 
+def part2(input):
+    rules, updates = extract_data(input)
+    count = 0
+
+    for update in updates:
+        if not is_valid(rules, update):
+            fixed_update = fix_update(rules, update)
+            count += fixed_update[len(update) // 2]
+
+    return count
+
 def main():
     input = read_input('input.txt')
 
     p1_ans = part1(input)
+    p2_ans = part2(input)
 
     print(f"Middle page count: {p1_ans}")
+    print(f"Part 2 middle page count: {p2_ans}")
 
 if __name__ == "__main__":
     main()
